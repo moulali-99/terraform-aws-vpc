6fe806ce4d3a8203e01f945132cecd1f3225ff1e
moulali pinjari <moulali22p@gmail.com>
2026-03-24 19:02:22 +0530

    actions

--- peering.tf
+++ peering.tf
@@ -30,7 +30,7 @@ resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id= aws_vpc_peering_connection.default.id
+  vpc_peering_connection_id= aws_vpc_peering_connection.default[count.index].id
 }
 resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0
